77dfd8a9f16173b0323fd38dea30b1a8df2ca431
victoria <pururpu@gmail.com>
2026-08-05 18:58:29 +0700

    Add comments to db_helper

--- fastapi-app/core/models/db_helper.py
+++ fastapi-app/core/models/db_helper.py
@@ -18,20 +18,22 @@ class DatabaseHelper:
             echo_pool=echo_pool,
             max_overflow=max_overflow,
             pool_size=pool_size,
-        )
+        ) # создаем движок бд
         self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
-        )
+        ) # создали объект, который создает сессии
 
     async def dispose(self) -> None:
         await self.engine.dispose()
+        # закрываем подключения, которые хранятся в пуле движка
 
     async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
         async with self.session_factory() as session:
             yield session
+        # асинхронный генератор, который создает и выдает сессию
 
 
 db_helper = DatabaseHelper(
