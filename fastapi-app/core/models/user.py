be8ca3c33d4755b27ebca57d9c312ed257073b45
victoria <pururpu@gmail.com>
2026-08-17 19:48:09 +0700

    add user table

---
+++ fastapi-app/core/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import mapped_column, Mapped
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (
+        UniqueConstraint("foo", "bar"),
+    )
