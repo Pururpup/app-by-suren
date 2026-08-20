15672ad6471589dc627fc9d230b7cdb4f729f960
victoria <pururpu@gmail.com>
2026-08-20 16:10:08 +0700

    create UserIdType var

--- fastapi-app/core/models/user.py
+++ fastapi-app/core/models/user.py
@@ -1,5 +1,6 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from core.models import Base
+from core.types.user_id import UserIdType
 
 from typing import TYPE_CHECKING
 
@@ -7,9 +8,7 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
-
+class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
     @classmethod
     def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, User)
